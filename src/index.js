ce813723ad007afcdfe9d384b74b0f8f6588817a
we.ralston <105259322+weralston@users.noreply.github.com>
2022-12-21 16:51:37 -0500

    Corrected the API URL correction

--- src/index.js
+++ src/index.js
@@ -31,6 +31,7 @@ function displayWeatherCondition(response) {
 function searchCity(city) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
 }
 
 function handleSubmit(event) {
@@ -42,6 +43,7 @@ function handleSubmit(event) {
 function searchLocation(position) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
 }
 
 function getCurrentLocation(event) {
