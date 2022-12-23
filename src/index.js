a87550a69d611f6dac74789e8dea540ba2b37b68
we.ralston <105259322+weralston@users.noreply.github.com>
2022-12-22 19:24:58 -0500

    Added API functions

--- src/index.js
+++ src/index.js
@@ -14,28 +14,53 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
-  let forecastHTML = `<div class="column">`;
   let days = ["Thu", "Fri", "Sat"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  let forecastHTML = `<div class="column">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
            <div class="col-6">
-             <div class = "weather-forecast-date"><strong>${day}</strong> </div> 
+             <div class="weather-forecast-date">${formatDay(
+               forecastDay.dt
+             )}</div> 
               </div>
               <div class = "weather-forecast-temperatures">
-                <span class ="weather-forecast-min">45°</span>
+                <span class ="weather-forecast-min">${Math.round(
+                  forecastDay.temp.min
+                )}</span>
                 /
-                <span class = "weather-forecast-max">60°</span>
+                <span class = "weather-forecast-max">${Math.round(
+                  forecastDay.temp.max
+                )}</span>
             </div>
           </div>
           `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "aca4dd3643b89e94dbd3cac6cf6f2638";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayWeatherCondition(response) {
@@ -50,6 +75,8 @@ function displayWeatherCondition(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
@@ -107,4 +134,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 searchCity("New York");
-displayForecast();
